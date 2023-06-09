941edbde7e2d12de9ffede456742e637c8e6f434
diego araujp <d-asantana@live.com>
2023-06-08 23:54:30 -0300

    add day selection logic

--- src/components/day/day-style.ts
+++ src/components/day/day-style.ts
@@ -1,15 +1,22 @@
 import styled from "styled-components";
 
 const dayBax = styled.div`
-  height: 8vw;
+  height: 5vw;
 
   display: flex;
   justify-content: center;
   align-items: center;
 
+  border: ${(props) => props.theme.isSelected && "solid 2px orange"};
   border-bottom: solid 2px orange;
   border-radius: 10px;
   margin: 5px 10px;
+
+  cursor: pointer;
+
+  :hover {
+    background-color: #303030;
+  }
 `;
 
 export const Style = { dayBax };
--- src/pages/calendar/calendar.tsx
+++ src/pages/calendar/calendar.tsx
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Style } from "./calendar-style.ts";
 import DayCell from "components/day";
 import WeekHead from "components/week-head-line";
 
 export const Calendar = () => {
+  const [count, setCount] = useState(Array());
   const monthMock = [...Array(30)];
+
+  const selectUnselectAction = (dayNumber: number) => {
+    if (!count.includes(dayNumber)) {
+      setCount([...count, dayNumber]);
+    } else {
+      setCount(count.filter((day) => day !== dayNumber));
+    }
+  };
 
   return (
     <>
-      <Style.CalendarBorder>
+      <Style.CalendarContainer>
         <WeekHead />
         {monthMock.map((x, dayNumber) => {
-          return <DayCell day={dayNumber + 1} />;
+          return (
+            <DayCell
+              key={dayNumber}
+              clickAction={() => selectUnselectAction(dayNumber)}
+              isSelected={count.includes(dayNumber)}
+              day={dayNumber + 1}
+            />
+          );
         })}
-      </Style.CalendarBorder>
+      </Style.CalendarContainer>
     </>
   );
 };
